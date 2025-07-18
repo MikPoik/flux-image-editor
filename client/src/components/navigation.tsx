c2af6e5a446d5e9063bb27eddb617d4c4e45e1ae
MikPoik <42643139+MikPoik@users.noreply.github.com>
2025-07-18 10:57:15 +0000

    name change

--- client/src/components/navigation.tsx
+++ client/src/components/navigation.tsx
@@ -56,7 +56,7 @@ export function Navigation() {
         {/* Logo */}
         <Link href="/">
           <h1 className="text-xl md:text-2xl font-bold text-foreground cursor-pointer">
-            Flux Image Editor
+            FluxaImage
           </h1>
         </Link>
 
